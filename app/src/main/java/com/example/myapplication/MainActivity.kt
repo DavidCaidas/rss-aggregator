6425fc64642a9ab57140ac110696e7bf673af78e
David <104197044+DavidCaidas@users.noreply.github.com>
2022-12-12 19:41:11 +0100

    añadidos permisos de acceso a internet y pruebas de funcionamiento

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.myapplication.data.remote.api.ApiClient
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import android.util.Log
+
 
 class MainActivity : AppCompatActivity() {
+
+    val apiClient = ApiClient()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        GlobalScope.launch {
+            try {
+                val model = apiClient.getRss("https://e00-marca.uecdn.es/rss/futbol/sevilla.xml")
+                Log.d("@dev", "model:$model")
+            } catch (ex: java.lang.Exception) {
+                Log.d("@dev-e", ex.message!!)
+            }
+        }
     }
 }
